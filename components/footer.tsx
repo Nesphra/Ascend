9b839d3aaa87f5f6f67318038b40a8a642363696
Romeo Beaurain <beaurainr@Romeos-MacBook-Air-2.local>
2025-04-13 17:29:08 -0500

    fixed some UI

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,31 +1,35 @@
 "use client";
 
-import { Text, Button, Link } from "@radix-ui/themes";
+import { Text, Button, Link, Separator } from "@radix-ui/themes";
 import Logo from "@/components/ui/logo";
 
 const Footer = () => {
   return (
-    <footer className="max-w-6xl w-full border-t border-foreground/10 flex justify-between items-center opacity-60 py-5">
+    <footer className="max-w-6xl w-full border-t border-foreground/10 flex flex-col md:flex-row justify-between items-center opacity-60 py-5 px-5">
         {/* Left Section */}
-        <div className="flex flex-col">
-          <div className="flex items-center gap-6">
+        <div className="flex flex-col w-full">
+          <div className="flex items-center gap-3">
             <div className="flex flex-row items-center">
                 <Logo/>
                 <Text weight="bold">Ascend</Text>
             </div>
+            <Separator orientation="vertical"/>
             <Button variant="outline" size="1" asChild>
                 <Link color="gray">Install Ascend</Link>
             </Button>
           </div>
-          <div className="text-xs translate-x-2">
+          <div className="text-xs translate-x-2 hidden md:flex">
             © Copyright 2024-2025, Romeo Beaurain. All rights reserved.
           </div>
         </div>
 
         {/* Right Section */}
-        <div className="flex flex-col items-start gap-1 text-xs">
+        <div className="flex flex-col items-start gap-1 text-xs whitespace-nowrap">
           <span>Facing issues? <Link href="/" weight="bold">Get help</Link></span>
           <span>Suggestions for improvement? <Link href="/" weight="bold">Share feedback</Link></span>
+          <div className="text-xs opacity-60 md:hidden">
+            © Copyright 2024-2025, Romeo Beaurain. All rights reserved.
+          </div>
         </div>
     </footer>
   );
